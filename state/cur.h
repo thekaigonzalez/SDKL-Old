a10302beead2c48f1c83f074a2986ae564d4ac0a
Kai-Builder <gkai70264@gmail.com>
2021-03-30 01:23:52 -0400

    insert Trees

---
+++ state/cur.h
@@ -0,0 +1,23 @@
+//
+// Created by seymo on 3/30/2021.
+//
+
+#ifndef SDK_L_CUR_H
+#define SDK_L_CUR_H
+#include "../tokenizer/types.h"
+#include "../except/exception.h"
+
+template<typename T>
+class static_switch {
+private:
+    SDK_Type default_type = NULLDATA;
+public:
+    /**
+     * Evaluates the default type into SDK Exceptions
+     * @return
+     */
+    const char* evaluate() {
+
+    }
+};
+#endif //SDK_L_CUR_H
---
+++ state/dom.h
@@ -0,0 +1,18 @@
+//
+// Created by seymo on 3/30/2021.
+//
+
+#ifndef SDK_L_DOM_H
+#define SDK_L_DOM_H
+enum TREE {
+    EPARSE,
+    RVALUE,
+    EVAL,
+    SEND
+};
+
+enum SYMBOL {
+    SDKCOMMENT,
+    SDK
+};
+#endif //SDK_L_DOM_H
