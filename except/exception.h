cb5f6813b51e74958c93d32b29cde52b72eeb294
Kai-Builder <gkai70264@gmail.com>
2021-03-30 01:24:03 -0400

    Exceptions and some overloaders

---
+++ except/exception.h
@@ -0,0 +1,8 @@
+# define NOTHANDLED                "Something wasn't handled."
+# define FAILEDTOPARSE                "Failed to Parse Expression."
+
+//Recent Standards
+
+# define NOTUPTOSTANDARDWARNING                "This Code may not be up to the standard. View the README.md For more information."
+// Evaluates a data type.
+extern Sdk_State<std::string>EvaluateDataType(SDK_Type datatype);
---
+++ except/get.h
@@ -0,0 +1,9 @@
+//
+// Created by seymo on 3/30/2021.
+//
+
+#ifndef SDK_L_GET_H
+#define SDK_L_GET_H
+#include "exception.h"
+
+#endif //SDK_L_GET_H
