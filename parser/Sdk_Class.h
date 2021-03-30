262c3cab36a8f215ba592d6886052a531acfc7e0
Kai-Builder <gkai70264@gmail.com>
2021-03-30 01:22:51 -0400

    SDK CLASS

---
+++ parser/Sdk_Class.h
@@ -0,0 +1,28 @@
+/* Array. */
+template<typename T>
+class Sdk_Class {
+private:
+    T Cls;
+    T& operator=(T Temp) {
+        Cls = Temp;
+    }
+    std::vector<T>Contents{};
+public:
+    T  shift() {
+        T c = Contents[0];
+        // Remove The First Content after pushing the original to a buffer
+        Contents[0] = '\0';
+        return c;
+    }
+    Sdk_Class<T>ListAll() {
+        return Contents;
+    }
+    T get(int index) {
+        return Contents[index];
+    }
+    void push(T C) {
+        Contents.push_back(C);
+    }
+
+
+};
