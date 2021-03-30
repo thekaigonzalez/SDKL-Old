a41d38b026ac432a61cba8391727bf1aa543fb22
Kai-Builder <gkai70264@gmail.com>
2021-03-30 01:22:02 -0400

    Support types

---
+++ tokenizer/types.h
@@ -0,0 +1,25 @@
+
+
+#ifndef SDK_L_TYPES_H
+#define SDK_L_TYPES_H
+
+// Defines basic typing for the SDK-L Standard
+
+enum SDK_Type {
+    STRING,
+    BASIC_STRING,
+    INTEGERVALUE,
+    NONE,
+    NOTHING,
+    VOIDIDENTIFIER,
+    VOIDLESS,
+    AIM,
+    FLOATINGPOINT,
+    DOUBLE,
+    NULLCLASS,
+    NULLDATA,
+    NULLPOINTER,
+    CHARACTER
+};
+
+#endif //SDK_L_TYPES_H
