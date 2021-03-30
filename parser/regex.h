f1d56bf8f4acca8f86881c73a13cfd33c9704d0c
Kai-Builder <gkai70264@gmail.com>
2021-03-30 01:23:18 -0400

    Insert Regular Expression Parsing capabilities

---
+++ parser/regex.h
@@ -0,0 +1,9 @@
+# define regex
+# define sep                '/'
+# define ssep                '\\'
+# define split_t                '+'
+/* Define Basic Controls for the parser. */
+
+extern void split(std::string);
+
+
