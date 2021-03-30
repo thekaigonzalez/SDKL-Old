f49e49ae40e1cc1de0370ee95fce4204d6e1d577
Kai-Builder <gkai70264@gmail.com>
2021-03-30 01:45:20 -0400

    Update the official reader

--- tokenizer/read.h
+++ tokenizer/read.h
@@ -4,7 +4,7 @@
 #ifndef SDK_L_READ_H
 #define SDK_L_READ_H
 
-#include <winnt.h>
+#include <cstring>
 #include "../parser/wildcard.h"
 #include "types.h"
 #include "attrib.h"
@@ -23,10 +23,74 @@ SDK_Type eval(const std::string& code) {
     if (keyword == "void" || keyword == "func") current_ReturnType = VOIDIDENTIFIER;
     if (keyword == ".call" || keyword == "vless") current_ReturnType = VOIDLESS;
     if (keyword == "null" || keyword == "nil") current_ReturnType = SDK_Type::NULLPOINTER;
+    /**
+     * IF the statment hasn't ended
+     */
     if (code.find(';') != std::string::npos)
-        std::cout << "Unexpected EOF When parsing. Expected ';' as EOS.";
+        std::cout << "Unexpected EOF When parsing. Expected ';' as EOS.\n";
+    /**
+     * If there's no callback methods
+     * @see SDK.CallbackMethod<typename>
+     */
+    if (code.find('.') != std::string::npos)
+        std::cout << "WARNING: No callback methods implemented. Nothing can be called.\n";
 }
-SDK_wildcard<SDK_Type> returnRaw (const std::string& code) {
+// This Part returns certain parts of a Statement using different tokens.
 
+/**
+ * app:help() => app:help()
+ * @param code
+ * @return
+ */
+std::string getFirstKeyword(const std::string& code) {
+    return code.substr(0, ' ');
+}
+/**
+ * app:help() => app:help
+ * @param code
+ * @param ignoreSpaces
+ * @return
+ */
+std::string getBeforeFunction(const std::string& code, bool ignoreSpaces) {
+
+}
+/**
+ * app:help() => app
+ * @param code
+ * @return
+ */
+std::string getDataTable(const std::string& code) {
+    return code.substr(0, ':');
+}
+/**
+ * Callback methods are explicitly defined methods that define a allocated and run script snippet.
+ * @code
+ * .call main() -> true
+ * call main() -> false
+ * @endcode
+ * @param snippet
+ * @return
+ */
+bool isCallbackMethod(const std::string& snippet) {
+    std::string first = getFirstKeyword(snippet);
+    if (strcmp(".", first.c_str()) != 0) {
+        if (first.find('(') != std::string::npos) {
+            std::cout << "Function is not a callback method or a valid function. Returning.\n";
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
+/**
+ * Checks if the snippet given is a function (1), And Is an official SDK Function (2).
+ * @param snippet
+ * @return true if snippet given is a function and an official one.
+ */
+bool isActualFunction(const std::string& snippet) {
+    if (snippet.find('(') != std::string::npos) {
+        std::cout << "Function is not a valid function. Nor Is it a SDK.Func to begin with.\n";
+    }
 }
 #endif //SDK_L_READ_H
