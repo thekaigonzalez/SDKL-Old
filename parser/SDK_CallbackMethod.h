8ac5142725656d49652f4430d1de77ec5e0fdf4d
Kai-Builder <gkai70264@gmail.com>
2021-03-30 16:34:02 -0400

    Allow Callback methods to be implemented in C

---
+++ parser/SDK_CallbackMethod.h
@@ -0,0 +1,14 @@
+//
+// Created by seymo on 3/30/2021.
+//
+
+#ifndef SDK_L_SDK_CALLBACKMETHOD_H
+#define SDK_L_SDK_CALLBACKMETHOD_H
+/**
+ * Implements a basic callback method.
+ */
+typedef struct {
+
+} SDK_CALLBACK;
+
+#endif //SDK_L_SDK_CALLBACKMETHOD_H
