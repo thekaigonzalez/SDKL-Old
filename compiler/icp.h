d19eda513f9ca7c8fdae2bb59682ea8e4f9b7f07
Kai-Builder <gkai70264@gmail.com>
2021-03-31 17:14:31 -0400

    Edit SCC

--- compiler/icp.h
+++ compiler/icp.h
@@ -5,4 +5,29 @@
 #ifndef SDK_L_ICP_H
 #define SDK_L_ICP_H
 
+#include <string>
+#include <iostream>
+#include <cstring>
+#include "../parser/SDK_CallbackMethod.h"
+#include "../parser/compile.h"
+
+
+SDK_CALLBACK_DECL compile(const std::string& code) {
+    SDKL::Compiler<SDK_CALLBACK_DECL> *mycompiler;
+
+    /**
+     * Checks if the Beginning keyword is a comment.
+     */
+    if (code.substr(0, ' ') == ">>") return mycompiler->comment();
+    /**
+     * Checks if the beginning keyword is a Block comment.
+     */
+    else if (strcmp(code.substr(0, ' ').c_str(), ">>[[") != 0) return mycompiler->null();
+    /**
+     * Else, Continue.
+     */
+    else {
+
+     }
+}
 #endif //SDK_L_ICP_H
