174ca9c66e8a77938fbd93e777e646e414f69b01
Kai-Builder <gkai70264@gmail.com>
2021-03-31 18:13:12 -0400

    compiler.cpp CLI ForeShadow

---
+++ compiler/compiler.cpp
@@ -0,0 +1,16 @@
+// Compiler CLI
+#include "bash.h"
+#include "cmd.h"
+#include "cref.h"
+#include "cxx.h"
+#include "htmltone.h"
+#include "icp.h"
+#include "js.h"
+
+
+
+
+int main()
+{
+    compile("code()");
+}
