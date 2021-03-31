30f9b1af4eb09876234f5fadb063fccf34610245
Kai-Builder <gkai70264@gmail.com>
2021-03-31 14:56:44 -0400

    Add some foreshadowing

---
+++ parser/native.h
@@ -0,0 +1,6 @@
+# define parser_native
+
+
+
+
+// allows scripts to be shared by sdk-l scripts.
---
+++ structinfo/script.h
@@ -0,0 +1,13 @@
+# define parser
+
+
+// This File is part of the SDKPL
+
+
+
+// Allow scripts to be shared.
+
+
+std::string includes(const std::string& code) {
+
+}
