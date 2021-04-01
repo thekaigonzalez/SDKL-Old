a67b11fa6319241cd0e40792c6f09ebac19f39b0
Kai-Builder <gkai70264@gmail.com>
2021-04-01 13:35:09 -0400

    Work on Appending to files, Update CMakeLists.txt

--- compiler/engine/append.h
+++ compiler/engine/append.h
@@ -4,7 +4,15 @@
 
 
 
-
 class SDKCompileFile {
-
+private:
+    std::string name;
+public:
+    void setName(const char* newname) {
+        name = newname;
+    }
+    void append(const char* text) {
+        std::ofstream ifile(name, std::ios::app);
+        ifile << text;
+    }
 };
