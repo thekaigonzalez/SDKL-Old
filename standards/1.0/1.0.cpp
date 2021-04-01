19640ecd248c277d564af8f9d4d6b9ca9077b9e2
Kai-Builder <gkai70264@gmail.com>
2021-04-01 15:51:39 -0400

    Update 1.0.cpp

--- standards/1.0/1.0.cpp
+++ standards/1.0/1.0.cpp
@@ -1,1 +1,14 @@
+#include <fstream>
 #include "grammar.h"
+
+
+
+int main(int argc, char* * argv) {
+    std::string file = argv[1];
+    std::string buffer;
+    std::string res;
+    std::ifstream ifile(file);
+    while (getline(ifile, buffer))
+        res += buffer;
+
+}
