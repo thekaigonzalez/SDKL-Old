04972f1346d248a87528c6b3a43e7516ad280842
Kai-Builder <gkai70264@gmail.com>
2021-04-01 15:51:56 -0400

    Update grammar.h

--- standards/1.0/grammar.h
+++ standards/1.0/grammar.h
@@ -1,3 +1,16 @@
 enum keyword {
-
+    entry,
+    newentry,
+    leaveentry,
+    winmain,
+    windl,
+    wstdcallback
 };
+
+/*
+ * Include SDK L Parsers
+ */
+#include "../../parser/parse.h"
+#include "../../tokenizer/stat.h"
+
+
