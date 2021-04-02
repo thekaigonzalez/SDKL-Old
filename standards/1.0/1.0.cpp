f423d1ea0954bbe909ee3a866532d59d29d376a4
Kai-Builder <gkai70264@gmail.com>
2021-04-02 11:17:25 -0400

    Finish the first Version

--- standards/1.0/1.0.cpp
+++ standards/1.0/1.0.cpp
@@ -1,8 +1,19 @@
 #include <fstream>
-#include "grammar.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include "../../tokenizer/read.h"
 
-
-
+std::string remove_indents(std::string TC) {
+    unsigned pos = TC.find("(");
+    std::string T = TC.substr(0, pos);
+    T.erase(std::remove_if(T.begin(), T.end(), isspace), T.end());
+    unsigned args = TC.find("(");
+    unsigned arg1 = TC.find(")");
+    std::string e = TC.substr(args, arg1);
+    T += e;
+    return T;
+}
 int main(int argc, char* * argv) {
     std::string file = argv[1];
     std::string buffer;
@@ -10,5 +21,17 @@ int main(int argc, char* * argv) {
     std::ifstream ifile(file);
     while (getline(ifile, buffer))
         res += buffer;
+
+    std::string reso = skipEntryPoints(res, BODY);
+
+    std::vector<std::string>FunctionsAlit(split(reso, "\n"));
+
+    for (const auto& func : FunctionsAlit) {
+
+
+        std::cout << remove_indents(func) << std::endl;
+        evaluateTrueFunctionalities(func);
+    }
+
 
 }
