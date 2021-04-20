004ac9325c1568c76fc8ddac918bb19a3bd3b404
Kai-Builder <gkai70264@gmail.com>
2021-04-20 12:36:45 -0400

    Upgrade Main.cpp to show recent updates

--- main.cpp
+++ main.cpp
@@ -5,26 +5,15 @@
 int main() {
     // Tests...
     // Separate functions like this
-    std::ifstream file("idiots.sdk");
+    std::ifstream file("index.sdk");
     std::string buffer;
     std::string resource;
 
     while (getline(file, resource)) {
-        buffer += resource;
+        SDKL_STREAM << resource;
     };
-    std::cout << "Buffeval = " << buffer << ", Evaluating All entries\n";
-        std::string body = skipEntryPoints(buffer, BODY);
 
-        std::cout << "Main Body:: " << body << std::endl;
-    std::string entry = skipEntryPoints(buffer, ENTRY);
-
-    std::cout << "Main Entry:: " << entry << std::endl;
-    std::string minap = getTrueParameters(buffer);
-
-    std::cout << "Main PARAMETERS:: " << minap << std::endl;
-    std::string minf = skipEntryPoints(buffer, MAIN);
-
-    std::cout << "Main function:: " << minf << std::endl;
+    parsestream();//new stream :)
 
 
 
