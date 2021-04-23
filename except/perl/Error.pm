eaef723d73ae2e15ad2d1cb3d34eec53f5d6481b
Kai-Builder <gkai70264@gmail.com>
2021-04-23 11:55:14 -0400

    Add a PERL API for SDKL Error formatting

---
+++ except/perl/Error.pm
@@ -0,0 +1,16 @@
+package Error;
+use strict;
+use warnings FATAL => 'all';
+
+1;
+
+sub new
+{
+    my $name;
+    my $is_accessible;
+
+}
+
+sub throw {
+    print "Failed to compile program after throwing instance of " + $_->{name};
+}
